6a78ec28395cabc2e4c2d3139608bbcceb38ec22
lucaslurf <lucas_faria@outlook.com>
2021-11-15 22:27:23 -0300

    adicionando desafios

---
+++ Projetos/Introduçao_Java/src/VariaveisLocais.java
@@ -0,0 +1,12 @@
+public class VariaveisLocais {
+
+    public static void main(String[] args){
+
+        System.out.println("olá, mundo!");
+        int idade = 27;
+        String nome = "Lucas";
+
+        System.out.println("Olá " + nome + ", você tem " + idade + " anos, seja bem vindo!" );
+
+    }
+}
---
+++ Projetos/desafios/src/com/company/TrigoNoTabuleiro.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class TrigoNoTabuleiro {
+
+    public static void main(String[] args) {
+        int gramasGraos = 12;
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i=0 ; i<n ; i++) {
+            int x = sc.nextInt();
+            double totalTrigo = (double) Math.pow(2, x) - 1;
+            double totalGramas = totalTrigo/gramasGraos;
+            long totalKilos = (long) (totalGramas/ 1000);
+
+
+            System.out.printf("%d kg\n", totalKilos);   //Complete o código aqui.
+        }
+        sc.close();
+
+    }
+        }
